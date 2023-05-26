2c0a43b95281ebdfb71534efd7a87f9f73b44a65
Yuxin Wang <541552695@qq.com>
2023-05-26 10:20:52 +0800

    RScript

--- RScript/013_Script_for_Rpackage.R
+++
@@ -1,4 +0,0 @@
-if(!require("devtools")) install.packages("devtools")
-if(!require("roxygen2")) install.packages("roxygen2")
-if(!require("testthat")) install.packages("testthat")
-usethis::create_package("./Rpackage/HBindex")
